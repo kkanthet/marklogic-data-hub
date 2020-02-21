023bd4967b00baa92f18abbae9286d0b8c4f26b0
kiran <kiran027@gmail.com>
2020-02-21 15:01:53 -0800

    Add creating users for unti tests

--- one-ui/src/test/java/com/marklogic/hub/oneui/TestHelper.java
+++ one-ui/src/test/java/com/marklogic/hub/oneui/TestHelper.java
@@ -10,6 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import com.marklogic.mgmt.ManageClient;
+import com.marklogic.mgmt.ManageConfig;
+import com.marklogic.mgmt.api.API;
+import com.marklogic.mgmt.api.security.User;
+import org.junit.jupiter.api.AfterAll;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -29,9 +36,22 @@ public class TestHelper {
     @Value("${test.dataHubDeveloperPassword:data-hub-developer-user}")
     public String dataHubDeveloperPassword;
 
-    @Value("${test.dataHubEnvironmentManagerUsername:data-hub-environment-manager-user}")
+    @Value("${test.adminUserName:admin}")
+    public String adminUserName;
+    @Value("${test.adminPassword:admin}")
+    public String adminPassword;
+
+    private API adminAPI;
+
+    private ManageClient client;
+
+    private User user;
+
+   // @Value("${test.dataHubEnvironmentManagerUsername:data-hub-environment-manager-user}")
+    @Value("data-hub-environment-manager-user")
     public String dataHubEnvironmentManagerUsername;
-    @Value("${test.dataHubEnvironmentManagerPassword:data-hub-environment-manager-user}")
+   // @Value("${test.dataHubEnvironmentManagerPassword:data-hub-environment-manager-user}")
+    @Value("data-hub-environment-manager-user")
     public String dataHubEnvironmentManagerPassword;
 
     public Path tempProjectDirectory = Files.createTempDirectory("one-ui-hub-project");
@@ -48,11 +68,13 @@ public class TestHelper {
     }
 
     public void authenticateSession() {
+        createUser(dataHubDeveloperUsername,dataHubDeveloperPassword,"data-hub-developer");
        EnvironmentInfo environmentInfo = new EnvironmentInfo(mlHost, "DIGEST", 8000,"DIGEST", 8002,"DIGEST", 8010, "DIGEST", 8011);
        hubConfig.setCredentials(environmentInfo, dataHubDeveloperUsername, dataHubDeveloperPassword);
     }
 
     public void authenticateSessionAsEnvironmentManager() {
+        createUser(dataHubEnvironmentManagerUsername,dataHubEnvironmentManagerPassword,"data-hub-environment-manager");
         EnvironmentInfo environmentInfo = new EnvironmentInfo(mlHost, "DIGEST", 8000,"DIGEST", 8002,"DIGEST", 8010, "DIGEST", 8011);
         hubConfig.setCredentials(environmentInfo, dataHubEnvironmentManagerUsername, dataHubEnvironmentManagerPassword);
     }
@@ -63,5 +85,22 @@ public class TestHelper {
             hubConfig.createProject(environmentService.getProjectDirectory());
             hubConfig.initHubProject();
         }
+    }
+
+    private void createUser(String username, String password, String role) {
+        client = new ManageClient();
+        client.setManageConfig(new ManageConfig(mlHost, 8002, adminUserName, adminPassword));
+        adminAPI = new API(client);
+
+        user = new User(adminAPI, username);
+        user.setUserName(username);
+        user.setPassword(password);
+        user.setRole(Stream.of(role).collect(Collectors.toList()));
+        user.save();
+    }
+
+    @AfterAll
+    private void deleteUser() {
+        user.delete();
     }
 }
